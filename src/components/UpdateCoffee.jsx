7767049af965564ddfb1ece174f72d70ebc1d31d
Arif2ankhi <arif2ankhi@gmail.com>
2024-11-28 15:20:20 +0000

    Updated coffee card

--- src/components/UpdateCoffee.jsx
+++ src/components/UpdateCoffee.jsx
@@ -35,7 +35,7 @@ const UpdateCoffee = () => {
         .then(res=> res.json())
         .then(data =>{
             console.log(data);
-            if(data.insertedId){
+            if(data.modifiedCount> 0){
                 Swal.fire({
                     title: 'Success!',
                     text: 'Coffee Updated Successfully',
